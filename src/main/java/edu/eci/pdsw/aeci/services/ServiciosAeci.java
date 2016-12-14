6c9c700b359da85292f917f295372b06314c53e9
Zekkenn <sebastian.moreno-r@mail.escuelaing.edu.co>
2016-12-13 23:06:05 -0500

    More documentation

--- src/main/java/edu/eci/pdsw/aeci/services/ServiciosAeci.java
+++ src/main/java/edu/eci/pdsw/aeci/services/ServiciosAeci.java
@@ -37,8 +37,8 @@ public abstract class ServiciosAeci implements Serializable{
     
     /**
      * Gives a instance for ServiciosAeci.
-     * @return insteance for ServiciosAeci.
-     * @throws RuntimeException if something wents wrong.
+     * @return instance for ServiciosAeci.
+     * @throws RuntimeException if something went wrong.
      */
     public static ServiciosAeciDAO getInstance() throws RuntimeException{        
         return instance;
@@ -68,20 +68,54 @@ public abstract class ServiciosAeci implements Serializable{
      * @param user the user to add.
      * @param graduate the graduate to add.
      * @param request the request to add.
-     * @throws ExcepcionServiciosAeci if something wents wrong.
+     * @throws ExcepcionServiciosAeci if something went wrong.
      */
     public abstract void addGraduateUser(User user, Graduate graduate, Request request) throws ExcepcionServiciosAeci;
     
+    /**
+     * Adds an user to the data base
+     * @param user The user to be added
+     * @throws ExcepcionServiciosAeci if something went wrong.
+     */
     public abstract void addUser(User user) throws ExcepcionServiciosAeci;
     
+    /**
+     * Adds an account to the data base
+     * @param account The account to be added
+     * @throws ExcepcionServiciosAeci if something went wrong.
+     */
     public abstract void addAccount(Account account)throws ExcepcionServiciosAeci;
     
+    /**
+     * Update an user given his id
+     * @param idUser The user's id
+     * @param user The user with new attributes 
+     * @throws ExcepcionServiciosAeci if idUser is null
+     */
     public abstract void updateUser(int idUser, User user) throws ExcepcionServiciosAeci;
 
+    /**
+     * Adds a request to the data base
+     * @param request The request to be added
+     * @throws ExcepcionServiciosAeci If the user of the request or his id is null
+     */
     public abstract void addRequest(Request request) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update a request in the data base, with a new commentary and state
+     * @param request The request with new attributes
+     * @param commentary The new commentary
+     * @param state The new state
+     * @throws ExcepcionServiciosAeci If the user of the request or his id is null
+     */
     public abstract void updateRequest(Request request, String commentary, String state) throws ExcepcionServiciosAeci;
     
+    /**
+     * Obtains the program given his id
+     * @param id The program's id
+     * @return The program
+     * @throws ExcepcionServiciosAeci if id is null
+     */
     public abstract Program getProgram(int id)throws ExcepcionServiciosAeci;
     
     /**
@@ -91,65 +125,171 @@ public abstract class ServiciosAeci implements Serializable{
      */
     public abstract List<Request> getPendingRequests() throws ExcepcionServiciosAeci;
     
+    /**
+     * Obtains a list with requests that aren't approved yet
+     * @return Return the list
+     * @throws ExcepcionServiciosAeci if occurs any error.
+     */
     public abstract List<Request> getRequestsSinAprobar() throws ExcepcionServiciosAeci;
     
+    /**
+     * Obtains the user given his id
+     * @param id The user's id
+     * @return The user searched
+     * @throws ExcepcionServiciosAeci if id is null.
+     */
     public abstract User getUser(int id)throws ExcepcionServiciosAeci;
     
+    /**
+     * Obtains the role given his id
+     * @param id The role's id
+     * @return The role
+     * @throws ExcepcionServiciosAeci If id is null
+     */
     public abstract Rol getRol(int id)throws ExcepcionServiciosAeci;
     
+    /**
+     * Update The user's first name given his id
+     * @param idUser The user's id
+     * @param firstName The new first name
+     * @throws ExcepcionServiciosAeci if idUser is null
+     */
     public abstract void updateUserFirstName(int idUser, String firstName) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update The user's last name given his id
+     * @param idUser The user's id
+     * @param lastName The new last name
+     * @throws ExcepcionServiciosAeci if idUser is null
+     */
     public abstract void updateUserLastName(int idUser, String lastName) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update The user's email given his id
+     * @param idUser The user's id
+     * @param email The new email
+     * @throws ExcepcionServiciosAeci if idUser is null
+     */
     public abstract void updateUserEmail(int idUser, String email) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update The user's phone given his id
+     * @param idUser The user's id
+     * @param phone The new phone
+     * @throws ExcepcionServiciosAeci id idUser is null
+     */
     public abstract void updateUserPhone(int idUser, String phone) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update The user's cell phone given his id
+     * @param idUser The user's id
+     * @param cellphone The new cell phone
+     * @throws ExcepcionServiciosAeci If idUser is null
+     */
     public abstract void updateUserCellphone(int idUser, String cellphone) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update the user's program given his id
+     * @param idUser The user's id
+     * @param program_Id The new program id
+     * @throws ExcepcionServiciosAeci If idUser is null or program_Id isn't in the data base or if it's null
+     */
     public abstract void updateUserProgram_Id(int idUser, int program_Id) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update the user's year graduate given his id
+     * @param idUser The user's id
+     * @param yearGraduate The new year graduate
+     * @throws ExcepcionServiciosAeci if idUser is null
+     */
     public abstract void updateUserYearGraduate(int idUser, int yearGraduate) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update the user's birth date given his id
+     * @param idUser The user's id
+     * @param birthDate The new birth date
+     * @throws ExcepcionServiciosAeci If idUser is null
+     */
     public abstract void updateUserBirthDate(int idUser, java.util.Date birthDate) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update the user's period of graduation given his id
+     * @param idUser The user's id
+     * @param period The new period
+     * @throws ExcepcionServiciosAeci if idUser is null
+     */
     public abstract void updateUserPeriod(int idUser, int period) throws ExcepcionServiciosAeci;
     
+    /**
+     * Update the user's role given his id
+     * @param idUser The user's id
+     * @param rol_Id The new role id
+     * @throws ExcepcionServiciosAeci If idUser is null or if rol_Id is null
+     */
     public abstract void updateUserRol_Id(int idUser, int rol_Id) throws ExcepcionServiciosAeci;
        
+    /**
+     * Add a new student into the data base
+     * @param student The new student
+     * @throws ExcepcionServiciosAeci If student id is null
+     */
     public abstract void addStudent(Student student) throws ExcepcionServiciosAeci;
     
+    /**
+     * Sets the new photo in the database
+     * @param student the student with a new photo of his student identification
+     * @throws ExcepcionServiciosAeci If student id is null
+     */
     public abstract void addCarnet(Student student)throws ExcepcionServiciosAeci;
     
+    /**
+     * Add a new graduate to the data base
+     * @param graduate The new graduate
+     * @throws ExcepcionServiciosAeci If the id of the graduate is null
+     */
     public abstract void addGraduate(Graduate graduate) throws ExcepcionServiciosAeci;
     
+    /**
+     * Add a new membership to the data base
+     * @param membership The new membership
+     * @throws ExcepcionServiciosAeci If the membership id is null
+     */
     public abstract void addMembership(Membership membership)throws ExcepcionServiciosAeci;
     
+    /**
+     * Update the membership payment, i.e, the image of the receipt and the payment number
+     * @param membership The membership to be updated
+     * @throws ExcepcionServiciosAeci If occurs any error
+     */
     public abstract void updatePayment(Membership membership)throws ExcepcionServiciosAeci;
     
     /**
      * Gives a list with all the memberships nearly to expire.
      * @return a list with all the memberships nearly to expire.
-<<<<<<< HEAD
      * @throws ExcepcionServiciosAeci if occurs any error
-=======
-     * @throws ExcepcionServiciosAeci if went something wrong
->>>>>>> d04c936f1e6903ec43c8d5be33e949e4d6588b1f
      */
     public abstract List<Membership> getSolicitudesPorVencerse()throws ExcepcionServiciosAeci;
     
     /**
-     * Gives a list with all the memberships that'r expire.
-     * @return a list with all the memberships that'r expire.
-<<<<<<< HEAD
+     * Gives a list with all the memberships that's expire.
+     * @return a list with all the memberships that's expire.
      * @throws ExcepcionServiciosAeci if occurs any error
-=======
-     * @throws ExcepcionServiciosAeci if something wrong happend
->>>>>>> d04c936f1e6903ec43c8d5be33e949e4d6588b1f
      */
     public abstract List<Membership> getAfiliacionesVencidas()throws ExcepcionServiciosAeci;
     
+    /**
+     * Obtains the rate given his id
+     * @param id The rate id
+     * @return The rate
+     * @throws ExcepcionServiciosAeci If the id is null
+     */
     public abstract Rate getRate(int id)throws ExcepcionServiciosAeci;
     
+    /**
+     * Obtains the account given his id
+     * @param id The account id
+     * @return The account
+     * @throws ExcepcionServiciosAeci if id is null
+     */
     public abstract Account getAccount(int id)throws ExcepcionServiciosAeci;
 }
